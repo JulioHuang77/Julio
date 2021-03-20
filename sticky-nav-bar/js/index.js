5587a0f20fd8b39514016ba6a064f8203e43d864
JulioHuang77 <julio53640@gmail.com>
2021-03-20 09:56:28 +0800

    upload files

---
+++ sticky-nav-bar/js/index.js
@@ -0,0 +1,17 @@
+/*
+
+Tinkering around with a fixed nav bar, and changing it based on scroll
+
+
+*/
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 49) {
+        $('body').addClass('header-fixed');
+    } else {
+        $('body').removeClass('header-fixed');
+    }
+  });
+});
